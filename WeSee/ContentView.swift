11d27b66890258ab36083054dbc50cef9125959d
haobenkun <haobenkun@enn.cn>
2026-06-09 11:05:58 +0800

    Initial Commit

---
+++ WeSee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSee
+//
+//  Created by haobenkun on 2026/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSee/WeSeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSeeApp.swift
+//  WeSee
+//
+//  Created by haobenkun on 2026/6/9.
+//
+
+import SwiftUI
+
+@main
+struct WeSeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
